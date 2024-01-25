abb9985069c67da9e31217f18dbb5d4b47a90e40
Rasmus <ahtavarasmus@gmail.com>
2024-01-25 21:57:55 +0200

    change

--- force/main.c
+++ force/main.c
@@ -15,55 +15,54 @@ int main(void)
     int leftLen = 0;
     int upLen = 0;
     int downLen = 0;
+    bool right = false;
+    bool left = false;
+    bool up = false;
+    bool down = false;
 
 
-    SetTargetFPS(30);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(30);
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
         if (IsKeyDown(KEY_RIGHT)) {
-            if (rightLen <= 10) rightLen++;
+            if (rightLen <= 10) rightLen++, leftLen--;
             rec.x += rightLen;
-            if (rec.x > screenWidth - rec.width) rec.x = screenWidth - rec.width;
-        } else if (IsKeyUp(KEY_RIGHT)){
-            if (rightLen > 0) rightLen--;
-            rec.x += rightLen;
-            if (rec.x > screenWidth - rec.width) rec.x = screenWidth - rec.width;
+            right = true;
+        } else {
+            right = false;
         }
         printf("rightLen: %d\n", rightLen);
         if (IsKeyDown(KEY_LEFT)) {
-            if (leftLen <= 10) leftLen++;
+            if (leftLen <= 10) leftLen++, rightLen--;
             rec.x -= leftLen;
-            if (rec.x < 0) rec.x = 0;
-        } else if (IsKeyUp(KEY_LEFT)){
-            if (leftLen > 0) leftLen--;
-            rec.x -= leftLen;
-            if (rec.x < 0) rec.x = 0;
+            left = true;
+        } else {
+            left = false;
         }
         printf("leftLen: %d\n", leftLen);
         if (IsKeyDown(KEY_UP)) {
-            if (upLen <= 10) upLen++;
-            rec.y -= upLen;
-            if (rec.y < 0) rec.y = 0;
-        } else if (IsKeyUp(KEY_UP)){
-            if (upLen > 0) upLen--;
-            rec.y -= upLen;
-            if (rec.y < 0) rec.y = 0;
+            if (upLen <= 10) upLen++, downLen--;
+            up = true;
+        } else {
+            up = false;
         }
         printf("upLen: %d\n", upLen);
         if (IsKeyDown(KEY_DOWN)) {
-            // speed is max 5 and kept in window
-            if (downLen <= 10) downLen++;
-            rec.y += downLen;
-            if (rec.y > screenHeight - rec.height) rec.y = screenHeight - rec.height;
-        } else if (IsKeyUp(KEY_DOWN)){
-            if (downLen > 0) downLen--;
-            rec.y += downLen;
-            if (rec.y > screenHeight - rec.height) rec.y = screenHeight - rec.height;
+            if (downLen <= 10) downLen++,upLen--;
+            down = true;
+        } else {
+            down = false;
         }
         printf("downLen: %d\n", downLen);
+        rec.x += rightLen - leftLen;
+        rec.y += downLen - upLen;
+        if (rec.x < 0) rec.x = 0;
+        if (rec.x > screenWidth - rec.width) rec.x = screenWidth - rec.width;
+        if (rec.y < 0) rec.y = 0;
+        if (rec.y > screenHeight - rec.height) rec.y = screenHeight - rec.height;
 
 
         BeginDrawing();
@@ -72,26 +71,20 @@ int main(void)
 
         DrawRectangleRec(rec, SKYBLUE);
         Vector2 center = {rec.x + rec.width / 2, rec.y + rec.height / 2};
-        Vector2 endPos = {center.x, center.y};
-        int arrowLen = 10;
-        if (upLen > 0) {
-            endPos.y -= arrowLen*upLen;
-            DrawLineEx(center, (Vector2){center.x, center.y - arrowLen*upLen}, 5.0f, GREEN);
+        if (up) {
+            DrawLineEx(center, (Vector2){center.x, center.y - 50}, 5.0f, GREEN);
         }
-        if (downLen > 0) {
-            endPos.y += 10*downLen;
-            DrawLineEx(center, (Vector2){center.x, center.y + arrowLen*downLen}, 5.0f, GREEN);
+        if (down) {
+            DrawLineEx(center, (Vector2){center.x, center.y + 50}, 5.0f, GREEN);
         }
-        if (leftLen > 0) {
-            endPos.x -= 10*leftLen;
-            DrawLineEx(center, (Vector2){center.x - arrowLen*leftLen, center.y}, 5.0f, GREEN);
+        if (left) {
+            DrawLineEx(center, (Vector2){center.x - 50, center.y}, 5.0f, GREEN);
         }
-        if (rightLen > 0) {
-            endPos.x += 10*rightLen;
-            DrawLineEx(center, (Vector2){center.x + arrowLen*rightLen, center.y}, 5.0f, GREEN);
+        if (right) {
+            DrawLineEx(center, (Vector2){center.x + 50, center.y}, 5.0f, GREEN);
         }
 
-        DrawLineEx(center, endPos, 5.0f, RED);
+        //DrawLineEx(center, endPos, 5.0f, RED);
 
         EndDrawing();
     }
